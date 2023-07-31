f76a6884eaa1b8bc556c0cc6675b0b5828841a48
Joshua | DC7IA <spam-github@dc7ia.eu>
2023-07-31 22:48:03 +0200

    fix copyright year and version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,11 +18,11 @@ sys.path.insert(0, os.path.abspath('.'))
 # -- Project information -----------------------------------------------------
 
 project = 'Matrix Betriebshandbuch DARC e.V.'
-copyright = '2022, Administrationsteam Matrix'
+copyright = '2023, Administrationsteam Matrix'
 author = 'Administrationsteam Matrix'
 
 # The full version, including alpha/beta/rc tags
-release = '0.7'
+release = '0.8'
 
 # -- General configuration ---------------------------------------------------
 
